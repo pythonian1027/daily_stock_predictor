2f77fccc61eccf80052e9e3e38055d8196905592
Ricardo <ricardocortez21@gmail.com>
2016-10-15 10:16:44 -0400

    self Q will be replaced to randomly populated matrix

--- machine-learning/projects/smartcab/smartcab/agent.py
+++ machine-learning/projects/smartcab/smartcab/agent.py
@@ -13,45 +13,44 @@ class LearningAgent(Agent):
         self.color = 'black'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         self.states_dict= dict()
-        self.Q = list()
-        self.gamma = 0.7 #Q learnging Gamma constant
+        self.Q = list() #list of tuples (a, b) where is the states and b are the actions
+        self.M = dict()
+        self.gamma = 0.7 #learning rate
         self.t = 1 #time initialization
         self.alpha = 1/self.t
-        self.action_idxs = zip(range(0,4), Environment.valid_actions)
-        
+        self.action_idxs = zip(range(0,4), Environment.valid_actions)        
         self.trial = 0
-#        self.L = list()
         self.total_trials = 100
         self.found = False
+        self.initialize_Q()  
         self.actionTaken = list()
-        self.initialize_Q        
+             
         # TODO: Initialize any additional variables here
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
-        self.trial +=1
-        if self.trial == 99:
-            pass
-#            print self.actionTaken
-            
-        self.calculateEpsilon()
+        self.trial +=1            
+        self.calculateEpsilon() # eps decay after every trial
         # TODO: Prepare for a new trip; reset any variables here, if required
     
+    #implementation of epsilon greedy algorithm 
     def calculateEpsilon(self, eps_0 = 0.9):
         eps = eps_0 - self.trial*(eps_0)/self.total_trials
         return eps                                        
         
+    #generates a state vector with random values        
     def randomInit(self):
         return [0.5*random.random() for i in xrange(4)]     
         
+    #not used at the moment but could be use to replace list Q        
     def initialize_Q(self):        
 #         'light', 'oncoming', 'right', 'left', 'next_waypoint'
-        s = [['red', 'yellow'], ['left', 'right','forward', None],\
+        s = [['red', 'green'], ['left', 'right','forward', None],\
         ['left', 'right','forward', None], ['left', 'right','forward', None], \
         ['right', 'left', 'forward'] ]
         listOfStates = list(itertools.product(*s))
         for state in listOfStates:
-            self.Q[state] = self.randomInit()
+            self.M[state] = self.randomInit()
                                               
     def update(self, t):
         self.t += 1        
@@ -66,99 +65,76 @@ class LearningAgent(Agent):
         curr_state = inputs
         curr_state['next_waypoint'] = self.planner.next_waypoint()
 #        states['deadline'] = self.env.get_deadline(self)
+        
+        #state <- ['light', 'oncoming', 'left', 'right', 'next_waypoint']
         self.state = curr_state
 
 
         # TODO: Select action according to your policy
-#        if self.alpha > 0.000025:
-                        
-#            self.L.append(self.use_random_action)        
+        #chooses a random action if random is lesser than currect epsilon
+        #otherwise chooses a learned action
         if random.random() < self.calculateEpsilon():
             action = random.choice(Environment.valid_actions)        
         else:
             for (a,b) in self.Q:
                 if a == curr_state:
-#                        print b
                     act_idx = b.index(max(b))
                     action = Environment.valid_actions[act_idx]   
                     self.actionTaken.append(action)          
-                    break        
-                    
+                    break                            
         try:                     
             action_idx = Environment.valid_actions.index(action)
         except NameError:
             action = random.choice(Environment.valid_actions)            
             action_idx = Environment.valid_actions.index(action)
-#            print " ...." + str(action)
-        
     
         # Execute action and get reward
         reward = self.env.act(self, action)
-#            print 'reward: ' + str(reward)
     ################ Q-learning equation ############################
     #Q (state, action) = R(state, action) + Gamma * Max[Q(next state, all actions)]
     
     #Q(1, 5) = R(1, 5) + 0.8 * Max[Q(5, 1), Q(5, 4), Q(5, 5)] = 100 + 0.8 * 0 = 100
-    #################################################################        
-#            print '*'*31
-        
+    #Q-learning equation V->(1-alpha)V + alpha(X)         
+    #################################################################            
         
         self.found = False
         for (a,b) in self.Q:
             if a == curr_state:
                 try:
                     b[action_idx] = (1 - self.alpha) * (b[action_idx]) +\
-                    (self.alpha) * (reward + self.gamma*max(self.Q[action_idx][1]))
-                
-#        for k,v in self.states_dict.iteritems():                   
-#            if curr_state == v:
-##                    print 'found an instance: ' + str(v)
-##                    self.L.append(self.use_random_action)
-#                try:
-##                        Q-learning equation V->(1-alpha)V + alpha(X)
-#                    self.Q[k][1][action_idx] = (1 - self.alpha) * (self.Q[k][1][action_idx]) +\
-#                    (self.alpha) * (reward + self.gamma*max(self.Q[action_idx][1]))
-#                    
+                    (self.alpha) * (reward + self.gamma*max(self.Q[action_idx][1]))                
+
                 except IndexError:
-                    # random initialization of action vector range [-0.5 2.5]
+                    # random initialization of action vector range
                     self.Q.append((curr_state, self.randomInit()))
                     b[action_idx] = (1 - self.alpha) * (b[action_idx]) +\
                     (self.alpha) * (reward + self.gamma*max(self.Q[-1][1]))
-#                    print "k: " + str(k)
-    #                self.Q[k] = q.append(zip(Environment.valid_actions, [0]*4))
                 self.found = True
                 break            
                 
-        if self.found == False:
-            print self.alpha
+        if self.found == False:         
 #                use random action on next iteration when curr_state is not found in dictionary of states
             #add the curr_state to dictionary of states
             self.states_dict[len(self.states_dict)] = curr_state   
             
             #initialize instance with 3-typle [state, actions, rewards]                
             self.Q.append((curr_state, self.randomInit()))  
-#                print self.Q
             try:
                 self.Q[-1][1][action_idx] = (1 - self.alpha) * (self.Q[-1][1][action_idx]) +\
                 (self.alpha) * (reward + self.gamma*max(self.Q[action_idx][1]))
     
             except IndexError:
-#                    the future action, Q[-2], is used to estimate Q[-1]
+#               the future action, Q[-2], is used to estimate Q[-1]
                 self.Q.append((curr_state, self.randomInit()))
                 self.Q[-2][1][action_idx] = (1 - self.alpha) * (self.Q[-2][1][action_idx]) +\
-                (self.alpha) * (reward + self.gamma*max(self.Q[-1][1]))                                
-    #            self.Q[-1][1][action_idx] = reward
+                (self.alpha) * (reward + self.gamma*max(self.Q[-1][1]))                                    
             
             
             
         # TODO: Learn policy based on state, action, reward
                     
-#        else:
-#            pass
-#        print len(self.Q)
-#        print len(self.states_dict)
-        
-                   
+
+
 #        print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
 
