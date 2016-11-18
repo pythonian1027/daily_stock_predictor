5328916fca2480d7db6a8e594259c0b1527f43ad
Ricardo <ricardocortez21@gmail.com>
2016-11-17 23:44:06 -0500

    added source from Python ML Blueprints textbook

--- machine-learning/projects/machineLearningTradingBackup/examples/python_ML_blueprints_Ch7.py
+++ machine-learning/projects/machineLearningTradingBackup/examples/python_ML_blueprints_Ch7.py
@@ -4,8 +4,32 @@ Created on Thu Nov 17 23:15:49 2016
 
 @author: rcortez
 """
-start_date = pd.to_datetime('2000-01-01')
+
+import pandas as pd
+import numpy as np
+from pandas_datareader import data, wb
+import matplotlib.pyplot as plt
+
+#%matplotlib inline
+pd.set_option('display.max_colwidth', 200)
+
+import pandas_datareader as pdr
+
+start_date = pd.to_datetime('2010-01-01')
 stop_date = pd.to_datetime('2016-03-01')
-sp = pdr.data.get_data_yahoo('SPY', start_date, stop_date)
 
+spy = pdr.data.get_data_yahoo('SPY', start_date, stop_date)
 
+spy_c = spy['Close']
+fig, ax = plt.subplots(figsize=(15,10))
+spy_c.plot(color='k')
+plt.title("SPY", fontsize=20)
+spy['Daily Change'] = pd.Series(spy['Close'] - spy['Open'])
+spy['Daily Change'].sum()
+np.std(spy['Daily Change'])
+spy['Overnight Change'] = pd.Series(spy['Open'] - spy['Close'].shift(1))
+np.std(spy['Overnight Change'])
+
+# daily returns
+daily_rtn = ((spy['Close'] - spy['Close'].shift(1))/spy['Close'].shift(1))*100
+daily_rtn
