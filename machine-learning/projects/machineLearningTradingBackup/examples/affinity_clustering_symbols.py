c28a6dd01ad1d53de620b757f393e17b41376e4a
Ricardo Cortez <ricardocortez21@gmail.com>
2016-12-21 14:10:48 -0500

    affinity working for S&P500

--- machine-learning/projects/machineLearningTradingBackup/examples/affinity_clustering_symbols.py
+++ machine-learning/projects/machineLearningTradingBackup/examples/affinity_clustering_symbols.py
@@ -15,6 +15,7 @@ from sklearn import covariance, cluster
 #from matplotlib.finance import quotes_historical_yahoo_ochl as quotes_yahoo
 import os
 import sys
+import pickle
 
 
 cwd = os.getcwd()
@@ -50,76 +51,106 @@ def get_data_all(symbol, dates):
 #    df = df.dropna(how='any')            
     return df
 
-def get_data(symbols, dates):
+def get_data(symbols, dates, base_dir):
     """Read stock data (adjusted close) for given symbols from CSV files."""
     df = pd.DataFrame(index=dates)
     if 'SPY' not in symbols:  # add SPY for reference, if absent
         symbols.insert(0, 'SPY')
 
-    for symbol in symbols:
-        df_temp = pd.read_csv(symbol_to_path(symbol), index_col='Date',
-                parse_dates=True, usecols=['Date', 'High', 'Adj Close'], na_values=['nan'])
-        df_temp = df_temp['Open'] - df_temp['Adj Close']
-        df = df.join(df_temp)
-        if symbol == 'SPY':  # drop dates SPY did not trade
-            df = df.dropna(subset=['SPY'])
-    return df
+    #read first from SPY to determine the size of the np array
+    df_temp = pd.read_csv(symbol_to_path('SPY'), index_col = 'Date', parse_dates = True, 
+        usecols = ['Date', 'Open', 'Adj Close'], na_values = ['nan'])
+    spy_len = df_temp.shape[0]
+    
+    np_delta = np.zeros(shape= (spy_len, len(symbols)) ) 
+    k_iter = enumerate(symbols)
+    num_cols = 0
+    for sym in k_iter:      
+        filename = sym[1] + '.csv'
+        if filename in os.listdir(base_dir):
+            if os.path.isfile(os.path.join(base_dir, filename)):
+#                print sym[1]
+                df_temp = pd.read_csv(symbol_to_path(sym[1]), index_col='Date',
+                    parse_dates=True, usecols=['Date', 'Open','Adj Close'], na_values=['nan'])
+                # if number of trading days differs from SPY then pass                     
+                if df_temp.shape[0] == spy_len :                     
+                    np_delta[:, num_cols ] = np.array(df_temp['Open'] - df_temp['Adj Close']).astype(np.float)
+                    num_cols += 1    
+                    
+                else: 
+                    pass                    
+                
+#                if df_temp.isnull().values.any():
+#                    pass
+#                else:
+#                    df = df.join(df_temp)
+#                    if symbol == 'SPY':  # drop dates SPY did not trade
+#                        df = df.dropna(subset=['SPY'])
+    return np_delta[:, 0:num_cols - 1]
 
 if __name__ == "__main__":
-#    run()
-    import pickle
-    #data contains a list of the S&P500 symbols
+    base_dir  = cwd + '/data/'
+    start_date = datetime.datetime(2014, 4, 5)
+    end_date = datetime.datetime(2015, 6, 2)   
+    dates = pd.date_range(start_date, end_date)
+
+#==============================================================================
+############## LOAD FROM symbol_map.json ######################################
+
+##     Input symbol file
+#    symbol_file = cwd+'/data/' + 'symbol_map.json'    
+#
+#
+##     Load the symbol map
+#    with open(symbol_file, 'r') as f:
+#        symbol_dict = json.loads(f.read())
+#
+#
+#    syms, names = np.array(list(symbol_dict.items())).T        
+#    symbols = list()           
+#    for s in syms:
+#        filename = s + '.csv'
+#        if os.path.isfile(os.path.join(base_dir, filename)):
+#            symbols.append(s)           
+#    quotes = [get_data_all(symbol, dates) for symbol in symbols]    
+#    
+##     Extract opening and closing quotes
+#    opening_quotes = np.array([quote['Open'] for quote in quotes]).astype(np.float)
+#    closing_quotes = np.array([quote['Close'] for quote in quotes]).astype(np.float)
+#    print type(opening_quotes)
+#    print (opening_quotes.shape)    
+#    
+#    # The daily fluctuations of the quotes 
+#    delta_quotes = closing_quotes - opening_quotes    
+#==============================================================================    
+#   data contains a list of the S&P500 symbols
     with open('sector_symbol_list.pkl', 'rb') as f:
         data = pickle.load(f)
     
     symbols = list()
     for k, v in data.iteritems():        
         for s in v:
-            symbols.append(s)
-    
-    
-        
+            symbols.append(s)     
 
-    start_date = datetime.datetime(2004, 4, 5)
-    end_date = datetime.datetime(2007, 6, 2)   
-    dates = pd.date_range(start_date, end_date)
-    base_dir  = cwd + '/data/'
+
     quotes = list()
-#    for item in os.listdir(base_dir):
-#        if os.path.isfile(os.path.join(base_dir, item)):
-#            if item.endswith('.csv'):
-##                print item
-#                quotes.append(get_data_all(item[:-4], dates))                
-        
-    # Input symbol file
-#    symbol_file = cwd+'/data/' + 'symbol_map.json'    
+    delta_quotes = get_data(symbols, dates, base_dir)
+    print delta_quotes.shape
+###    for item in os.listdir(base_dir):
+###        if os.path.isfile(os.path.join(base_dir, item)):
+###           if item.endswith('.csv'):
+###                quotes.append(get_data_all(item[:-4], dates))                
+       
 
+                
+    num_symbols = 200
+    delta_quotes = delta_quotes[:, :num_symbols]
+    symbols = symbols[:num_symbols]
+    delta_quotes = delta_quotes.T
+    
+    names = np.array(symbols)
+#==============================================================================      
 
-    # Load the symbol map
-#    with open(symbol_file, 'r') as f:
-#        symbol_dict = json.loads(f.read())
-#
-#    symbols, names = np.array(list(symbol_dict.items())).T        
-                
-#    for symbol in symbols:
-#        try:                
-#            quotes.append(get_data_all(symbol, dates))
-#        except:
-#            print "symbol: {} does not exists".format(symbol)                    
-            
-#    quotes = [get_data_all(symbol, dates) for symbol in symbols]    
-#  len  print quotes
-#        
-                
-    
-    
-    
-    # Extract opening and closing quotes
-    opening_quotes = np.array([quote['Open'] for quote in quotes]).astype(np.float)
-    closing_quotes = np.array([quote['Close'] for quote in quotes]).astype(np.float)
-    
-    # The daily fluctuations of the quotes 
-    delta_quotes = closing_quotes - opening_quotes
     
     # Build a graph model from the correlations
     edge_model = covariance.GraphLassoCV()
@@ -132,6 +163,7 @@ if __name__ == "__main__":
     with np.errstate(invalid='ignore'):
         edge_model.fit(X)
     
+    print 'here'
     # Build clustering model using affinity propagation
     _, labels = cluster.affinity_propagation(edge_model.covariance_)
     num_labels = labels.max()
